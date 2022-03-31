8e43fee9725478e16a413fcd98c0ceb8edaff378
nhutluong <Nhut.Luong@mscoding.org>
2022-03-31 13:30:37 -0500

    added calcScripts.js

--- js/calcScripts.js
+++ js/calcScripts.js
@@ -1,1 +1,43 @@
-﻿
+﻿//CREATE ClASS
+class Calculator {
+    constructor() {
+        this.displayValue = '';
+    }//end constructor();
+
+}//end function
+
+//CREATE INSTANCE OF THE CLASS THIS INSTANCE IS GLOBAL
+webCalculator = new Calculator;
+
+//EVENTS
+function InputDigit(digit) {
+    webCalculator.displayValue += digit;
+    RefreshScreen();
+}//end function
+
+function InputOperator(digit) {
+    webCalculator.displayValue += " " + digit + " ";
+    RefreshScreen();
+}//end function
+
+function ResetCalculator() {
+    webCalculator.displayValue = '';
+    RefreshScreen();
+}//end function
+
+//ALTER THE CALCULATOR'S SCREEN
+function RefreshScreen() {
+    //GRAB THE ELEMENT THAT REPRESENTS THE SCREEN <INPUT>
+    var inputElement = document.querySelector('.calculator-screen');
+
+    //CHANGE THE VALUE PROPERTY OF THE ELEMENT
+    inputElement.value = webCalculator.displayValue;
+}//end function
+
+//PROCESS INPUT ON CALCULATOR SCREEN
+function ProcessInput() {
+    //TO DO
+    var answer = 56;
+    webCalculator.displayValue = answer;
+    RefreshScreen();
+}//end function
